17bece0d0c15153d0b424b32184bd11e1c1cdda6
Anchal Chaudharyyy <134596545+anchalsingh2929@users.noreply.github.com>
2023-06-22 20:49:09 +0530

    day 10 practice

---
+++ day10/12.py
@@ -0,0 +1,5 @@
+a=float(input("enter your number -- "))
+if a>12 and a<20:
+  print("it is a digit btw 12 and 20")
+else:
+  print("it is not btw 12 and 20")
---
+++ day10/vowel.py
@@ -0,0 +1,5 @@
+a=input("enter your alpha -- ")
+if a=="a" or a=="i" or a=="e" or a=="o" or a=="u":
+   print("its a vowel")
+else:
+   print("its a consonent")
