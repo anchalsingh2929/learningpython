49794ae4d4fccf824bb28f0a04d1f8dbc60b2f21
Anchal Chaudharyyy <134596545+anchalsingh2929@users.noreply.github.com>
2023-06-25 11:17:37 +0530

    day 12 practice

---
+++ day12/calculator.py
@@ -0,0 +1,13 @@
+a=int(input("type your no. --"))
+b=int(input("type your no. --"))
+c=input(" what operation do you want ?? --")
+if c=="multiply" or c=="*":
+  print(a*b)
+elif c=="divide" or c=="/":
+  print(a/b)
+elif c=="addition" or c=="+":
+  print(a+b)
+elif c=="subtraction" or c=="-":
+  print(a-b)
+else: 
+   print("error")
---
+++ day12/weekday.py
@@ -0,0 +1,17 @@
+n=int(input("enter your no. --"))
+if n==1:
+   print("its monday")
+elif n==2:
+   print("its tuesday")
+elif n==3:
+   print("its wednesday")
+elif n==4:
+   print("its thruesday")
+elif n==5:
+   print("its friday")
+elif n==6:
+   print("its saturday")
+elif n==7:
+   print("its sunday")
+else:
+   print("no week day found")
