bafe064685f0d1f062a013eb7d6da2bdee0b20fc
Anchal Chaudharyyy <134596545+anchalsingh2929@users.noreply.github.com>
2023-08-25 21:38:08 +0530

    Add files via upload

---
+++ day27/date.py
@@ -0,0 +1,8 @@
+
+while True:
+  a=int(input("enter your date-- "))
+  if a==21:
+    print("yeah its a coding day ")
+    break
+
+   
---
+++ day27/loop.py
@@ -0,0 +1,7 @@
+a=int(input("enter your no.-- "))
+i=1
+p=0
+while i<=a:
+  p=p+i
+  i=i+1
+print(p)
