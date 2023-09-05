f7b83d74f7adda0f34b0ea777f5071e8ded24e7c
Anchal Chaudhary <anchalchaudhary2929@gmail.com>
2023-09-05 22:47:25 +0530

    practice

---
+++ day31/count.py
@@ -0,0 +1,21 @@
+a=input("enter name - ")
+b=input("enter character- ")
+i=0
+r=0
+while i<len(a):
+  
+  if (a[i]==b):
+     r=r+1 
+  i=i+1
+print(r)
+  
+    
+  #blue
+ # u
+ # b, 
+
+
+
+
+
+
---
+++ day31/greatersmallerstr.py
@@ -0,0 +1,6 @@
+a=input(" enter your str - ")
+b=input("enter your str - ")
+if len(a)>len(b):
+     print(a)
+else :
+      print(b)
