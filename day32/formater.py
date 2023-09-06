7584091cf5987619736a10d637a270446faaadcb
Anchal Chaudhary <anchalchaudhary2929@gmail.com>
2023-09-06 22:06:30 +0530

    practice

---
+++ day32/formater.py
@@ -0,0 +1,9 @@
+a, b = 2, 3
+#print("A " , a , "+ B " ,b , " = " , a + b)
+print(f"A {a} + B {b} = {a+b}")
+#A 2 + B 3 = 5
+
+
+print(f"{a} {35}")
+print(f"{1}{a}{a}{a}{a}")
+#12222
---
+++ day32/printfeature.py
@@ -0,0 +1,2 @@
+print("is ths is a " "test string")
+#is ths a test string
