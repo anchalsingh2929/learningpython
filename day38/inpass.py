1df82b5100cb189de58af4423c81de8ef2bf6fa8
Anchal Chaudhary <anchalchaudhary2929@gmail.com>
2023-10-19 03:15:08 +0530

    practice of day38

---
+++ day38/inpass.py
@@ -0,0 +1,9 @@
+
+while True:
+ a=input("pass-")
+ if a=="boyfrnd" or a=="girlfrnd" :
+    print("allowed") 
+    break
+ else:
+      print("not allowed")
+      print("try again")
---
+++ day38/password.py
@@ -0,0 +1,6 @@
+a=input("pass -")
+if a=="babyboi":
+  print("you are allowed")
+else :
+   print("password seekh k aa")
+
