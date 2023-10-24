8542c9278bc89b39a6f2c8461c27f937eaa40465
Anchal Chaudhary <anchalchaudhary2929@gmail.com>
2023-10-25 03:44:53 +0530

    practice day38

---
+++ day38/intro.py
@@ -0,0 +1,1 @@
+print("hello my name is anchal \t im from india and im a learner i study bsc .\t now im persuing a programming lang course in python \n there are 4 members in my family..")
---
+++ day38/palindromalgorithm.py
@@ -0,0 +1,3 @@
+1.take a string as an input
+2.reverse this string and store in a variables
+3.if the reverse string is equal to the input string then that string is palindrom if not then its not . 
