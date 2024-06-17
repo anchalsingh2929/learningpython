d2f55cefbf0769c41ae17e88bee7a1d1717239ba
Anchal Chaudhary <anchalchaudhary2929@gmail.com>
2024-06-17 12:58:22 +0530

    adding my day48 practice

---
+++ day48/fizzbuzz.py
@@ -0,0 +1,7 @@
+a=int(input("enter no -"))
+if a%3==0 and a%5==0:
+    print("fizzbuzz")
+elif  a%5==0:
+    print('buzz')
+elif a%3==0:
+    print("fizz")       
---
+++ day48/prime.py
@@ -0,0 +1,7 @@
+a=57
+for x in range(2,a):
+  if  a % x==0:
+        print("not prime")
+        break
+else :
+  print("its a prime")    
