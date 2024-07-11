1b66685ddcaf6b486288d1653e879b57604357f4
Anchal Chaudhary <anchalchaudhary2929@gmail.com>
2024-07-11 14:42:12 +0530

    practising pytest

---
+++ pytest/firsttest.py
@@ -0,0 +1,17 @@
+def func(x):
+    return x + 1
+
+
+def test_answer11():
+    assert func(2) == 5
+
+def test_answer12():
+    assert func(8) == 5
+
+
+def test_answer13():
+    assert func(4) == 5
+
+
+def test_answer14():
+    assert func(6) == 5
