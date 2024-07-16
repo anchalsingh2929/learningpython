d5908a819f638947245b210bdf89f66e9f5e50fb
Anchal Chaudhary <anchalchaudhary2929@gmail.com>
2024-07-16 12:19:41 +0530

    automation

--- playwright/google.py
+++ playwright/google.py
@@ -1,9 +1,12 @@
 import time
 from playwright.sync_api import Page, expect
 
-def test_has_title(page: Page):
-    page.goto("https://google.com/")
-    page.get_by_role("a", title="Search").click()
+def test_has_title(page):
+    page.goto("https://youtube.com/")
+    page.get_by_placeholder("Search", exact=True).fill("lyfofvipin_tech")
+    time.sleep(3)
+    page.get_by_role("button",name="Search",exact=True).click()
+    page.get_by_role("button",name="Search",exact=True).click()
+    time.sleep(6.7)
 
-    time.sleep(5)
-
+  
