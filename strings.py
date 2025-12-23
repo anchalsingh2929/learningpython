df3ad03cadefd2c01579e1e42fbe7eecaf7b6aad
Anchal Chaudharyyy <134596545+anchalsingh2929@users.noreply.github.com>
2025-12-24 01:05:35 +0530

    addding String practice

---
+++ strings.py
@@ -0,0 +1,81 @@
+# # # my_strings="--i make vlogs on yoyutub--"
+# # # print(my_strings.strip("-"))
+# # # output = "i make logs on youtube"
+
+# # my_strings= "--Gorgeous--"
+# # print(my_strings.lstrip("-"))
+# # output= "Gorgeous--"
+
+
+# my_string="--Gorgeous--"
+# print(my_string.rstrip("-"))
+# output = "--Gorgeous"
+
+
+# my_strings = "https://anchal.com/temp/vlogs"
+# print(my_strings.lstrip("httpsabcd"))
+# outup = "://anchal.com/temp/vlogs"
+
+# my_strings = "https://anchal.com/temp/vlogs"
+# print(my_strings.split("/")[2])
+# output = "anchal.com"
+
+
+# my_strings = "i am just a girl"
+# print(my_strings.startswith("am"))
+# output = False 
+
+
+
+# my_strings = "i am just a girl"
+# print(my_strings.endswith("girl"))
+# output = True
+
+
+# my_strings = "Anchal"
+# print(my_strings.ljust(10,"*"))
+# output = Anchal****
+
+
+# my_strings="Anchal"
+# print(my_strings.rjust(15,"@"))
+# outpit = @@@@@@@@@Anchal
+
+
+
+# my_strings = "name Alice"
+# print(my_strings.replace("Alice","Anchu"))
+# output= name Anchu
+
+
+# my_strings = "i am a player"
+# print(my_strings.find("p"))
+# output= 7
+
+# my_strings = "i am a player hello hello "
+# print(my_strings.count("hello"))
+#  output= 2
+
+
+# my_strings = "0123456789"
+# print(my_strings[1:10:2])   
+# #output = 13579
+
+ 
+# print(my_string[ -8:-1 ])   
+# Output: 2345678
+
+
+
+# print(my_string[ -8::-1 ]) 
+# Output: 210
+
+
+# print(my_string[ -5:1:-1 ])   
+# Output: 5432
+
+
+# print(my_string[ -5:-1:-1 ])   
+# Output: <blank>
+
+
